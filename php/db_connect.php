b251c543665834ffad879dde9d2f7fde93fbe373
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2020-10-18 13:39:36 +0200

    Connexion à la base de donnée

--- php/db_connect.php
+++ php/db_connect.php
@@ -3,5 +3,6 @@
 	$username = "root";
 	$password = "";
 	$db = "hackathon";
-	$mysqli = mysqli_connect($server, $username, $password, $db);
+	$mysqli = mysqli_connect($server, $username, $password, $db );
+	$mysqli->set_charset("utf8")
 ?>
